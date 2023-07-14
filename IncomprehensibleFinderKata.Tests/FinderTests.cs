b39f10a5c26965a981dfd85fc259101130e36c0d
Reza Biglari <reza.biglari@sky.uk>
2023-07-14 13:06:35 +0100

    change: use MSTest instead of XUnit

--- IncomprehensibleFinderKata.Tests/FinderTests.cs
+++ IncomprehensibleFinderKata.Tests/FinderTests.cs
@@ -1,73 +1,74 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
-using Xunit;
 
 namespace IncomprehensibleFinderKata.Tests {
+    [TestClass]
     public class FinderTests {
-        [Fact]
+        [TestMethod]
         public void Returns_Empty_Results_When_Given_Empty_List() {
             var list = new List<Thing>();
             var finder = new Finder(list);
 
             var result = finder.Find(FT.One);
 
-            Assert.Null(result.P1);
-            Assert.Null(result.P2);
+            Assert.IsNull(result.P1);
+            Assert.IsNull(result.P2);
         }
 
-        [Fact]
+        [TestMethod]
         public void Returns_Empty_Results_When_Given_One_Person() {
             var list = new List<Thing>() {sue};
             var finder = new Finder(list);
 
             var result = finder.Find(FT.One);
 
-            Assert.Null(result.P1);
-            Assert.Null(result.P2);
+            Assert.IsNull(result.P1);
+            Assert.IsNull(result.P2);
         }
 
-        [Fact]
+        [TestMethod]
         public void Returns_Closest_Two_For_Two_People() {
             var list = new List<Thing>() {sue, greg};
             var finder = new Finder(list);
 
             var result = finder.Find(FT.One);
 
-            Assert.Same(sue, result.P1);
-            Assert.Same(greg, result.P2);
+            Assert.AreEqual(sue, result.P1);
+            Assert.AreEqual(greg, result.P2);
         }
 
-        [Fact]
+        [TestMethod]
         public void Returns_Furthest_Two_For_Two_People() {
             var list = new List<Thing>() {greg, mike};
             var finder = new Finder(list);
 
             var result = finder.Find(FT.Two);
 
-            Assert.Same(greg, result.P1);
-            Assert.Same(mike, result.P2);
+            Assert.AreEqual(greg, result.P1);
+            Assert.AreEqual(mike, result.P2);
         }
 
-        [Fact]
+        [TestMethod]
         public void Returns_Furthest_Two_For_Four_People() {
             var list = new List<Thing>() {greg, mike, sarah, sue};
             var finder = new Finder(list);
 
             var result = finder.Find(FT.Two);
 
-            Assert.Same(sue, result.P1);
-            Assert.Same(sarah, result.P2);
+            Assert.AreEqual(sue, result.P1);
+            Assert.AreEqual(sarah, result.P2);
         }
 
-        [Fact]
+        [TestMethod]
         public void Returns_Closest_Two_For_Four_People() {
             var list = new List<Thing>() {greg, mike, sarah, sue};
             var finder = new Finder(list);
 
             var result = finder.Find(FT.One);
 
-            Assert.Same(sue, result.P1);
-            Assert.Same(greg, result.P2);
+            Assert.AreEqual(sue, result.P1);
+            Assert.AreEqual(greg, result.P2);
         }
 
         Thing sue = new Thing() {Name = "Sue", BirthDate = new DateTime(1950, 1, 1)};
